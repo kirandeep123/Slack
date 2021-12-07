c8657424e3531a7a9460921890762fa142a54b07
kirandeep123 <me@kiran.cc>
2021-12-07 18:57:09 +0530

    [feat] slack basic setup

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnSr-oL6P6j1cgUIdDCUYckAgrS4Sw42g",
+  authDomain: "slack-c613a.firebaseapp.com",
+  projectId: "slack-c613a",
+  storageBucket: "slack-c613a.appspot.com",
+  messagingSenderId: "975025727534",
+  appId: "1:975025727534:web:d001064ee32df18c4ed7bf",
+  measurementId: "G-6D0FY0CWKP"
+};
